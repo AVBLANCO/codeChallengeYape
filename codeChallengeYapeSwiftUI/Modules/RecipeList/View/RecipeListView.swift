1eb1ebd577e39a3ee2fe94a4e148e99d67a3b730
Victor Manuel Blanco Mancera <BLANKO_1490@HOTMAIL.COM>
2024-05-14 22:25:01 -0500

    generate component into view sned element viewmodel to view for buld the view.

--- codeChallengeYapeSwiftUI/Modules/RecipeList/View/RecipeListView.swift
+++ codeChallengeYapeSwiftUI/Modules/RecipeList/View/RecipeListView.swift
@@ -12,17 +12,94 @@ import CoreData
 struct RecipeListView: View {
     @ObservedObject var viewModel: RecipeListViewModel
 
-    init(viewModel: RecipeListViewModel) {
-        self.viewModel = viewModel
-    }
-
     var body: some View {
         NavigationView {
-            List(viewModel.recipes) { recipe in
-                Text(recipe.nombre ?? "")
+            List(viewModel.recipes, id: \.id) { recipe in
+                NavigationLink(destination: DetailView(recipe: recipe)) {
+                    RecipeRow(recipe: recipe)
+                }
             }
             .navigationBarTitle("Recetas")
+            .onAppear {
+                viewModel.fetchRecipes()
+            }
         }
+    }
+}
+
+struct RecipeRow: View {
+    @StateObject private var imageLoader = ImageLoader()
+    var recipe: RecipeEntity
+
+    var body: some View {
+        HStack {
+            AsyncImage(url: recipe.imagenReceta ?? "")
+                .frame(width: 100, height: 100)
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading) {
+                Text(recipe.nombre ?? "")
+                    .font(.headline)
+                Text(recipe.descripcion ?? "")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            .padding(.leading, 8)
+            
+            Spacer()
+        }
+        .padding(8)
+    }
+}
+
+struct AsyncImage: View {
+    @ObservedObject private var imageLoader: ImageLoader
+    var url: String
+    
+    init(url: String) {
+        self.url = url
+        _imageLoader = ObservedObject(wrappedValue: ImageLoader())
+        if let imageUrl = URL(string: url) {
+            imageLoader.loadImage(from: imageUrl)
+        }
+    }
+    
+    var body: some View {
+        if let image = imageLoader.image {
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFit()
+        } else {
+            Image(systemName: "photo")
+                .resizable()
+                .scaledToFit()
+        }
+    }
+}
+
+class ImageLoader: ObservableObject {
+    @Published var image: UIImage?
+    @Published var error: Error?
+    
+    func loadImage(from url: URL) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    self.error = error
+                }
+                return
+            }
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    self.error = NSError(domain: "com.example.ImageLoader", code: -1,
+                                         userInfo: [NSLocalizedDescriptionKey: "No data received"])
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                self.image = UIImage(data: data)
+            }
+        }.resume()
     }
 }
 
--- codeChallengeYapeSwiftUI/Modules/RecipeList/ViewModel/RecipeListViewModel.swift
+++ codeChallengeYapeSwiftUI/Modules/RecipeList/ViewModel/RecipeListViewModel.swift
@@ -20,22 +20,48 @@ class RecipeListViewModel: ObservableObject {
     }
     
     func fetchRecipes() {
-        guard let url = URL(string: "https://66340f2a9bb0df2359a0a1b3.mockapi.io/recetas") else { return }
+        guard let url = URL(string: "https://66340f2a9bb0df2359a0a1b3.mockapi.io/recetas") else {
+            print("URL inválida")
+            return
+        }
 
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data else { return }
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                print("Error de red: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("Respuesta de red inválida")
+                return
+            }
+            
+            guard (200...299).contains(httpResponse.statusCode) else {
+                print("Error de servidor: Código de estado \(httpResponse.statusCode)")
+                return
+            }
+            
+            guard let data = data else {
+                debugPrint("No se pudo obtener datos")
+                return
+            }
+            
+            if let jsonString = String(data: data, encoding: .utf8) {
+               // debugPrint("JSON recibido: \(jsonString)")
+            }
+
             do {
                 let recipes = try JSONDecoder().decode([RecipeResponse].self, from: data)
-                //debugPrint("REsponse \(recipes)")
                 DispatchQueue.main.async {
                     // Save recipes to CoreData
+                    debugPrint("response \(recipes)")
                     self.model.saveRecipiesFromEndPoint(recipes)
                 }
             } catch {
-                print("Error decoding JSON: \(error)")
+                print("Error decodificando JSON: \(error)")
             }
         }.resume()
     }
-
-
 }
